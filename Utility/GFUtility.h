0fc7d54b7c21c3d165fcef4d76ff15c640792e94
Vitaliy Glamozda <www.vitaliy_14@mail.ru>
2021-10-13 22:07:55 +0300

    Fixed multiplication in GF256

--- Utility/GFUtility.h
+++ Utility/GFUtility.h
@@ -7,9 +7,6 @@ namespace GF256
 {
     uint8_t Add(uint8_t A, uint8_t B);
     uint8_t Multiply(uint8_t A, uint8_t B);
-    uint8_t Divide(uint8_t A, uint8_t B);
-
-    void GenerateTables();
 }
 
 #endif // GFUTILITY_H
